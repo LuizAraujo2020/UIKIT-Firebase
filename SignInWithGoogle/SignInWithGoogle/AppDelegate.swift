446fcbf841d93d797745d70ac79ba4e97c3ee74a
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2023-01-21 11:57:16 -0300

    sign in and restore google

--- SignInWithGoogle/SignInWithGoogle/AppDelegate.swift
+++ SignInWithGoogle/SignInWithGoogle/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import FirebaseCore
+import GoogleSignIn
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,8 +16,32 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         FirebaseApp.configure()
         
+        GIDSignIn.sharedInstance.restorePreviousSignIn { user, error in
+          if error != nil || user == nil {
+            // Show the app's signed-out state.
+          } else {
+            // Show the app's signed-in state.
+          }
+        }
+        return true    }
+    
+    func application(
+      _ app: UIApplication,
+      open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]
+    ) -> Bool {
+      var handled: Bool
+
+        handled = GIDSignIn.sharedInstance.handle(url)
+      if handled {
         return true
+      }
+
+      // Handle other custom URL types.
+
+      // If not handled by this app, return false.
+      return false
     }
+    
 
     // MARK: UISceneSession Lifecycle
 
