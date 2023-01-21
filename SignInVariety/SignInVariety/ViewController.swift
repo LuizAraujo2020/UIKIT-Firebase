b7276b037a4ccf7da12daf73b0857ee17f5cc77b
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2023-01-21 10:57:29 -0300

    init commit SignInVariety

---
+++ SignInVariety/SignInVariety/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SignInVariety
+//
+//  Created by Luiz Araujo on 21/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
